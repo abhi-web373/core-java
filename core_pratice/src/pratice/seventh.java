b9845474ac442aa0998db9c0dda9d681a1bf3b6b
abhi-web373 <abhijeetshinde7500@gmail.com>
2026-09-01 11:47:18 +0530

    7

---
+++ core_pratice/src/pratice/seventh.java
@@ -0,0 +1,24 @@
+package pratice;
+
+public class seventh {
+
+	public static void main(String[] args) {
+
+		        int num = 1;       
+		        int counter = 0;
+
+		        while (num <= 100)   
+		        {
+		            if (num % 5 == 0)
+		            {
+		                counter++;   
+		            }
+
+		            num++;           
+		        }
+
+		        System.out.println(counter);
+		    }
+
+	}
+
