bf9cd1bc3c55d7116ab9746c7ca8a6f70bcb5dc7
abhi-web373 <abhijeetshinde7500@gmail.com>
2026-09-02 11:34:32 +0530

    eight

---
+++ core_pratice/src/pratice/eight.java
@@ -0,0 +1,21 @@
+package pratice;
+
+public class eight {
+	
+	public static void main(String[] args) {
+		int num = 500;     
+        int counter = 0;
+
+        while (num <= 1000) 
+        {
+            if (num % 2 != 0)
+            {
+                counter++;  
+            }
+
+            num++;          
+        }
+
+        System.out.println(counter);
+    }
+}
