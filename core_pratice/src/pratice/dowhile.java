d5f4b6e157e5eb867804638b4740a45ff8eea16f
abhi-web373 <abhijeetshinde7500@gmail.com>
2026-09-04 11:56:37 +0530

    dowhile

---
+++ core_pratice/src/pratice/dowhile.java
@@ -0,0 +1,19 @@
+package pratice;
+
+public class dowhile {
+
+	public static void main(String[] args) {
+		
+		int age=7;
+		
+		do {
+			System.out.println("you can enter");
+			
+		}while(age>18);
+		
+		System.out.println("the end of programm!");
+	
+
+	}
+
+}
