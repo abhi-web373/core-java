d42b00a0ec37fe49d0f4b0afd26c1f01c58be35b
abhi-web373 <abhijeetshinde7500@gmail.com>
2026-09-08 12:01:47 +0530

    int

---
+++ core_pratice/src/pratice_java/Array.java
@@ -0,0 +1,21 @@
+package pratice_java;
+
+public class Array {
+
+	public static void main(String[] args) {
+
+		int[] arr = new int[5];
+		arr[0] = 10;
+		arr[1] = 90;
+		arr[2] = 70;
+		arr[3] = 50;	
+		arr[4] = 40;
+		System.out.println(arr[0]);
+		System.out.println(arr[1]);
+		System.out.println(arr[2]);
+		System.out.println(arr[3]);
+		System.out.println(arr[4]);
+		System.out.println(arr.length);
+		System.out.println(arr[4]*5);
+	}
+}
