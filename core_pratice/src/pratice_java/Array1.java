60fa4ff78ab9af04055a56718213604996ec82b0
abhi-web373 <abhijeetshinde7500@gmail.com>
2026-09-08 12:02:52 +0530

    Char

---
+++ core_pratice/src/pratice_java/Array1.java
@@ -0,0 +1,20 @@
+package pratice_java;
+
+public class Array1 {
+
+	public static void main(String[] args) {
+
+		char[] arr=new char[6];
+		
+		arr[0]= 'A';
+		arr[1]= 'B';
+		arr[2]= 'C';
+		arr[3]= 'D';
+		arr[4]= 'E';
+		arr[5]= 'F';
+		
+		System.out.println(arr[0]);
+		System.out.println(arr.length);
+	}
+	
+}
