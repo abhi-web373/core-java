cf6bd5484ba2842281c0fbb01ccce4af3c49180f
abhi-web373 <abhijeetshinde7500@gmail.com>
2026-09-15 22:32:50 +0530

    Array6

---
+++ core_pratice/src/pratice_java/Array6.java
@@ -0,0 +1,48 @@
+package pratice_java;
+public class Array6 {
+    public static void main(String[] args) {
+
+        int[] numbers = {45, 7, 18, 23, 50, 13, 91, 29, 40, 17};
+
+        int i = 0;
+        int sum = 0;
+        int highest = numbers[0];
+
+        while (i < numbers.length)
+        {
+            int num = numbers[i];
+
+            // Find highest value
+            if (num > highest)
+            {
+                highest = num;
+            }
+
+            // Check prime number
+            int j = 2;
+            int count = 0;
+
+            while (j < num)
+            {
+                if (num % j == 0)
+                {
+                    count++;
+                }
+
+                j++;
+            }
+
+            // Print prime and calculate sum
+            if (count == 0 && num > 1)
+            {
+                System.out.println(num);
+                sum = sum + num;
+            }
+
+            i++;
+        }
+
+        System.out.println("Sum of Prime Numbers = " + sum);
+        System.out.println("Highest Value = " + highest);
+    }
+}
