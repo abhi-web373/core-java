1ff2cd48367d2c445dd7b07f5be30a1058117dbe
abhi-web373 <abhijeetshinde7500@gmail.com>
2026-09-17 22:50:14 +0530

    2

---
+++ core_pratice/src/pratice_java/Second2.java
@@ -0,0 +1,25 @@
+package pratice_java;
+
+public class Second2 {
+
+	public static void main(String[] args) {
+
+		int[][] arr=new int[3][3];
+		
+		for(int i=0;i<3;i++)
+		{
+			for(int j=0;j<arr[i].length;j++)
+			{
+				arr[i][j]=10;
+			}
+		}
+		for(int i=0; i<3; i++) {
+			
+			for(int j=0; j<arr[i].length; j++) {
+			
+		        System.out.print(arr[i][j] + " ");
+		    }
+		    System.out.println();
+		}
+}
+}
